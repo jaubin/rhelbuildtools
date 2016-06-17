74c2a23e5b2396b0bf434488b7d6fa3d7ed2de7d
jaubin <julien_aubin_lm@yahoo.fr>
2016-06-17 17:54:36 +0200

    Added script to increment stuff

---
+++ gojulrpmbuildtools/scripts/incrementPackageVersion.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+SCRIPT_NAME=$(basename $0)
+
+exitWithError()
+{
+   echo >&2 "$1"
+   exit $2
+}
+
+usage()
+{
+   cat >&2 <<-EOF
+
+   USAGE $SCRIPT_NAME
+
+   This script will look into the project-info.properties
+   in the current directory and increment the project.version entry
+   by one on its last digit.
+
+EOF
+}
+
+performIncrement()
+{
+   [ -f project-info.properties ] || exitWithError "No project-info.properties in this directory !" 1
+
+   local projectVersion=$(grep "^project.version=" project-info.properties)
+   
+   local projectVersionPrefix=${projectVersion%[0-9]*}
+   local projectVersionSuffix=${projectVersion##*.}
+
+   ((projectVersionSuffix++))
+
+   sed -i "s/^project.version=.*/${projectVersionPrefix}${projectVersionSuffix}/" project-info.properties
+}
+
+performIncrement
